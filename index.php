088812e1a3c2bef92f70414a1b570ab859b758ed
Jackson Silva <163166933+ogJackson1@users.noreply.github.com>
2024-05-10 14:40:23 -0300

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'Produto.class.php';
-
-$produto = new Produto();
-$descricao =  'notebook';
-$preco = 2.500;
-$fornecedor = "dell";
-$produto -> inserirProduto($descricao, $preco, $fornecedor);
