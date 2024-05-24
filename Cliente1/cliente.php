d55a0b578f0b6b062427e6b6a447e629f6eb0fb6
Jackson Silva <163166933+ogJackson1@users.noreply.github.com>
2024-05-24 14:36:00 -0300

    Add files via upload

---
+++ Cliente1/cliente.php
@@ -0,0 +1,38 @@
+<?php
+  class Cliente{
+     
+    private int $id;
+    private String $nome;
+    private String $email;
+
+    public function getId(){
+        return $this->id;
+    }
+
+    public function getNome(){
+        return $this->nome;
+    }
+
+    public function getEmail(){
+        return $this->email;
+    }
+
+    public function setId($id){
+       $this->id;
+  }
+
+  public function setNome($nome){
+       $this->nome;
+  }
+
+  public function setEmail($email){
+      $this->email;
+  }
+
+
+
+   
+    
+  }
+   ?>
+
---
+++ Cliente1/index.php
@@ -0,0 +1,8 @@
+<?php
+
+require 'cliente.php';
+
+$cliente = new cliente();
+$cliente->setNome("Paulo");
+$cliente->setId("2000-00");
+$cliente->setEmail("paulo.epdf@gmail.coms");
