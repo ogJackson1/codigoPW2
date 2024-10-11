22e05357fcb9408e6d0a8f237c91e0d20608ad41
Jackson Silva <163166933+ogJackson1@users.noreply.github.com>
2024-10-11 15:21:32 -0300

    Delete Produto directory

--- Produto/Index.php
+++
@@ -1,5 +0,0 @@
-<?php
-require 'Produto.class.php';
-$p=new Produto(); // $p recebe uma nova classe chamada Produto
-$p -> conectar();
-
--- Produto/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'Produto.class.php';
-$p=new Produto();
-$p -> setDescricao('computador Lenovo');
-$dados= $p -> getDescricao();
-
-echo '<h2>O contéudo do atributo descrição é: '.$dados;
